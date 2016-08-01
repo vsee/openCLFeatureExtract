7ce02e0678c10ed4f63a09a1c33aeb4376137b08
Volker Seeker <s1160444@sms.ed.ac.uk>
2016-08-01 21:11:15 +0100

    walking through module implemented

--- oclFeatureExt.cpp
+++ oclFeatureExt.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <memory>
 #include <iostream>
+#include <unordered_set>
 
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
@@ -11,22 +12,65 @@
 using namespace std;
 using namespace llvm;
 
+/* TODO
+ * count loads and stores total, global and local
+ * 
+ * count compute operations 
+ * Binary Ops: add, fadd, sub, fsub, mul, fmul, udiv, sdif, fdiv, urem, srem, frem
+ * Bitwise Binary Ops: shl, lshr, ashr, and, or xor
+ * vector ops: extractelement, insertelement, shufflevector
+ * aggregate ops: extractvalue, insertvalue
+ */
+
+static const unordered_set<string> BIN_OPS = {"add","fadd"};
+
 int main(int argc, char* argv[]) {
 
-    LLVMContext context;
     const string fileName = "res/kernelSample/nw.bc";
+    
     ErrorOr<unique_ptr<MemoryBuffer>> fileBuffer = MemoryBuffer::getFile(fileName);
     if (error_code ec = fileBuffer.getError())
-		cout << "ERROR loading bitcode file: " << fileName << " -- " << ec.message() << endl;
+		outs() << "ERROR loading bitcode file: " << fileName << " -- " << ec.message() << "\n";
 	else
-		cout << "Bitcode file loaded: " << fileName << endl;  
+		outs() << "Bitcode file loaded: " << fileName << "\n";  
     
+    
+    LLVMContext context;
     MemoryBufferRef memRef = (*fileBuffer)->getMemBufferRef();
     ErrorOr<unique_ptr<Module>> bcModule = parseBitcodeFile(memRef, context);
     if (error_code ec = bcModule.getError())
-            cout << "ERROR parsing bitcode file" << ec.message() << endl;
+            outs() << "ERROR parsing bitcode file" << ec.message() << "\n";
     else
-            cout << "Bitcode parsed successfully!"<< endl;
+            outs() << "Bitcode parsed successfully!"<< "\n";
     
+    int bbcount = 0;
+    int funcount = 0;
+    int instcount = 0;
+       
+    for (Module::const_iterator curFref = (*bcModule)->getFunctionList().begin(),
+         endFref = (*bcModule)->getFunctionList().end();
+         curFref != endFref; curFref++) {
+        funcount++;
+        outs() << "Found function: " << curFref->getName() << "\n";
+ 
+        for (Function::const_iterator curBref = curFref->begin(), endBref = curFref->end();
+         curBref != endBref; curBref++) {
+            bbcount++;
+            outs() << "Found a block: " << curBref->getName() << "\n";
+            
+            for (BasicBlock::const_iterator curIref = curBref->begin(), endIref = curBref->end();
+                 curIref != endIref; curIref++) {
+                instcount++;
+                outs() << "Found an instruction: " << *curIref << "\n";
+            }
+            
+        }
+        
+        
+    }
     
+    outs() << "\n###############################\n";
+    outs() << "#Functions: " << funcount << "\n";
+    outs() << "#Basic Blocks: " << bbcount << "\n";
+    outs() << "#Instructions: " << instcount << "\n";
 }
